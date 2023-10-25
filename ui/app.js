33fbe406d7cb620037c623b84a87a84f34cdaa1f
hennycraft52 <122104659+Hennycraft52@users.noreply.github.com>
2023-10-25 17:14:32 +0200

    Update app.js

--- ui/app.js
+++ ui/app.js
@@ -1,1 +1,14 @@
-
+// JavaScript-Code für das Webinterface
+document.addEventListener("DOMContentLoaded", function() {
+  // Lade die Liste der Plugins vom Server und füge sie zur Seite hinzu
+  fetch("/api/plugins")
+    .then(response => response.json())
+    .then(data => {
+      const pluginList = document.getElementById("plugin-list");
+      data.plugins.forEach(plugin => {
+        const listItem = document.createElement("li");
+        listItem.textContent = plugin.name;
+        pluginList.appendChild(listItem);
+      });
+    });
+});
