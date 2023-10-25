c708dc959cbc95bcdb57c8036a01c0cf34072502
hennycraft52 <122104659+Hennycraft52@users.noreply.github.com>
2023-10-25 18:08:45 +0200

    Update app.js

--- ui/app.js
+++ ui/app.js
@@ -1,14 +1,38 @@
-// JavaScript-Code für das Webinterface
 document.addEventListener("DOMContentLoaded", function() {
-  // Lade die Liste der Plugins vom Server und füge sie zur Seite hinzu
-  fetch("/api/plugins")
-    .then(response => response.json())
-    .then(data => {
-      const pluginList = document.getElementById("plugin-list");
-      data.plugins.forEach(plugin => {
-        const listItem = document.createElement("li");
-        listItem.textContent = plugin.name;
-        pluginList.appendChild(listItem);
-      });
-    });
+  const pluginList = document.getElementById("plugin-list");
+  const createPluginForm = document.getElementById("create-plugin-form");
+
+  // Dummy-Daten für installierte Plugins (ersetze dies durch echte Daten)
+  const installedPlugins = [
+    "Plugin 1",
+    "Plugin 2",
+  // ...
+
+  // Plugins in die Liste einfügen
+  installedPlugins.forEach(function(pluginName) {
+    const listItem = document.createElement("li");
+    listItem.textContent = pluginName;
+    pluginList.appendChild(listItem);
+  });
+
+  // Event-Handler für das Erstellen von Plugins
+  createPluginForm.addEventListener("submit", function(event) {
+    event.preventDefault(); // Verhindert das Neuladen der Seite
+
+    const pluginNameInput = document.getElementById("plugin-name");
+    const newPluginName = pluginNameInput.value;
+
+    // Hier kannst du den Code hinzufügen, um das neue Plugin zu erstellen
+    // Dies könnte eine AJAX-Anfrage an den Server sein
+    // Beachte, dass dies ein vereinfachtes Beispiel ist
+    // und das Erstellen eines Plugins in der Praxis komplexer sein kann
+
+    // Nachdem das Plugin erstellt wurde, füge es zur Liste hinzu
+    const listItem = document.createElement("li");
+    listItem.textContent = newPluginName;
+    pluginList.appendChild(listItem);
+
+    // Leere das Eingabefeld
+    pluginNameInput.value = "";
+  });
 });
